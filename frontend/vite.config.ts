956a3f1398885fd1397370c14cc9e39e221d0343
Stefano Vittorio Porta <stefa168@hotmail.it>
2026-07-24 20:53:17 +0200

    chore: aggiunto supporto per le icone

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		// https://svelte.dev/docs/kit/images#sveltejs-enhanced-img-Setup
+		// deve necessariamente comparire prima del plugin di svelte!
+		enhancedImages(),
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		})
+	],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
